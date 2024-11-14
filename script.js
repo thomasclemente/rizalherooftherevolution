0fcb0ff8e91c1f29c8d158a7c315034bce123b33
thomasclemente <150893263+thomasclemente@users.noreply.github.com>
2024-11-15 00:34:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+// Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click
+());
+
+//download file
+function downloadFile() {
+    // Create a hidden link to trigger the download
+    var link = document.createElement('a');
+    link.href = 'https://drive.google.com/uc?export=download&id=1lm6jPHmV-ROa5mIF2XKH0guiDCJmT0ei'; // Your direct download URL
+    link.download = ''; // Optional: you can specify a filename here
+    link.click(); // Trigger the download
+}
