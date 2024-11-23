e4e8391c3266c85c768f50774edac457eb792b14
thomasclemente <150893263+thomasclemente@users.noreply.github.com>
2024-11-23 21:19:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ menuCloseButton.addEventListener("click", () => menuOpenButton.click
 function downloadFile() {
     // Create a hidden link to trigger the download
     var link = document.createElement('a');
-    link.href = 'https://drive.google.com/uc?export=download&id=1lm6jPHmV-ROa5mIF2XKH0guiDCJmT0ei'; // Your direct download URL
+    link.href = 'https://drive.google.com/uc?export=download&id=1xFA-cXF8Y9beIHNFhaVqGXdBBMB3Nbw3'; // Your direct download URL
     link.download = ''; // Optional: you can specify a filename here
     link.click(); // Trigger the download
 }
